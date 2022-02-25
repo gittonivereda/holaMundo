fb151427885c8376ee071e0bacabb29e19082e79
anlodios <kiring@gmail.com>
2022-02-25 14:00:23 +0100

    Es mi segundo commit c

---
+++ src/No.java
@@ -0,0 +1,7 @@
+public class No {
+    private  String respuesta;
+    public No(String resp){
+        this.respuesta=resp;
+        System.out.println(this.respuesta);
+    }
+}
--- src/hola.java
+++ src/hola.java
@@ -1,7 +1,8 @@
 public class hola {
 
-    public static void main (String []args){
-        System.out.println("adios mundo");
+    public static void main (String [] args){
+        System.out.println("adios mundo cruel");
+        No hola=new No("NO");
     }
 
 }
